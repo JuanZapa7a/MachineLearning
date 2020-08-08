b83dacc3686ee84bdef3641f1fea5a725b4ce19f
juanzapata <juan.zapata@upct.es>
2020-08-08 10:26:19 +0200

    Original Commit

--- Chapter17_Save_and_Load_Machine_Learning_Models.py
+++ Chapter17_Save_and_Load_Machine_Learning_Models.py
@@ -54,6 +54,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import joblib
 
+
 # load data
 filename = 'pima-indians-diabetes.data.csv'
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
